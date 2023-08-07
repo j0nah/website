131d6d283c9e1b2c86b06d88ae6c212e62d1bb57
Jonah Glover <jonah+j0nah@jonahglover.com>
2023-08-07 14:44:29 -0700

    first commit

---
+++ gatsby-config.ts
@@ -0,0 +1,219 @@
+import type { GatsbyConfig, PluginRef } from "gatsby"
+import "dotenv/config"
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    // You can overwrite values here that are used for the SEO component
+    // You can also add new values here to query them like usual
+    // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.mjs
+    siteTitle: `Stuff I don't want to forget`,
+    siteTitleAlt: `Stuff I don't want to forget`,
+    siteHeadline: `Stuff I don't want to forget`,
+    siteUrl: `https://j0nah.com`,
+    siteDescription: `A website I made to document stuff I learn and don't want to forget.`,
+    siteImage: `/banner.jpg`,
+    siteLanguage: `en`,
+    author: `Jonah`,
+  },
+  trailingSlash: `never`,
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      // See the theme's README for all available options
+      options: {
+        mdx: false,
+        navigation: [
+          {
+            title: `Blog`,
+            slug: `/blog`,
+          },
+          {
+            title: `Projects`,
+            slug: `/projects`,
+          },
+          {
+            title: `About`,
+            slug: `/about`,
+          },
+        ],
+        externalLinks: [
+          {
+            name: `Github`,
+            url: `https://github.com/j0nah`,
+          },
+          // {
+          //   name: `Homepage`,
+          //   url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+          // },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
+        short_name: `minimal-blog`,
+        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        start_url: `/`,
+        background_color: `#fff`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#6B46C1`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title: siteTitle
+                description: siteDescription
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({
+              query: { site, allPost },
+            }: {
+              query: { allPost: IAllPost; site: { siteMetadata: ISiteMetadata } }
+            }) =>
+              allPost.nodes.map((post) => {
+                const url = site.siteMetadata.siteUrl + post.slug
+                const content = `<p>${post.excerpt}</p><div style="margin-top: 50px; font-style: italic;"><strong><a href="${url}">Keep reading</a>.</strong></div><br /> <br />`
+
+                return {
+                  title: post.title,
+                  date: post.date,
+                  excerpt: post.excerpt,
+                  url,
+                  guid: url,
+                  custom_elements: [{ "content:encoded": content }],
+                }
+              }),
+            query: `{
+  allPost(sort: {date: DESC}) {
+    nodes {
+      title
+      date(formatString: "MMMM D, YYYY")
+      excerpt
+      slug
+    }
+  }
+}`,
+            output: `rss.xml`,
+            title: `Stuff I don't want to forget - j0nah.com`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+          lessBabel: true,
+          extensions: [`.mdx`, `.md`],
+          gatsbyRemarkPlugins: [
+              {
+                  resolve: `gatsby-remark-images`,
+                  options: {
+                      maxWidth: 960,
+                      quality: 90,
+                      linkImagesToOriginal: false,
+                      backgroundColor: `transparent`,
+                  },
+              },
+              {
+                resolve: `gatsby-remark-table-of-contents`,
+                options: {
+                  exclude: "Table of Contents",
+                  tight: false,
+                  ordered: false,
+                  fromHeading: 2,
+                  toHeading: 2,
+                  className: "table-of-contents"
+                },
+              },
+              {
+                  resolve: `gatsby-remark-autolink-headers`,
+                  options: {
+                      className: `header-link-icon`,
+                      maintainCase: false,
+                  }
+              }
+          ],
+      },
+    },
+    // You can remove this plugin if you don't need it
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-statoscope`,
+      options: {
+        saveReportTo: `${__dirname}/public/.statoscope/_bundle.html`,
+        saveStatsTo: `${__dirname}/public/.statoscope/_stats.json`,
+        open: false,
+      },
+    },
+  ].filter(Boolean) as Array<PluginRef>,
+}
+
+export default config
+
+interface IPostTag {
+  name: string
+  slug: string
+}
+
+interface IPost {
+  slug: string
+  title: string
+  defer: boolean
+  date: string
+  excerpt: string
+  contentFilePath: string
+  html: string
+  timeToRead: number
+  wordCount: number
+  tags: Array<IPostTag>
+  banner: any
+  description: string
+  canonicalUrl: string
+}
+
+interface IAllPost {
+  nodes: Array<IPost>
+}
+
+interface ISiteMetadata {
+  siteTitle: string
+  siteTitleAlt: string
+  siteHeadline: string
+  siteUrl: string
+  siteDescription: string
+  siteImage: string
+  author: string
+}
